0a7f8e185805c38ed0a3f0afc6d7d00787ba2ad4
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-05 10:26:34 +0800

    add AdapterView OnItemSelectedListener

--- 20240805- Spinner/app/src/main/java/com/example/a20240805_spinner/MainActivity.java
+++ 20240805- Spinner/app/src/main/java/com/example/a20240805_spinner/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a20240805_spinner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void buttonClick(View view)
+    {
+        //Get String resource
+        Spinner sp=(Spinner)findViewById(R.id.spinner);
+        //Get option array
+        String [] fruits = getResources().getStringArray(R.array.fruit);
+        int index =sp.getSelectedItemPosition();
+        TextView result=(TextView)findViewById(R.id.tvResult);
+        result.setText("您選的是 :"+fruits[index]);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
