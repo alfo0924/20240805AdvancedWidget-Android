84534e01c7adece1492f596855c3db7becd1431d
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-07 10:12:25 +0800

    v1.21 add ListView

--- 20240807ListViewTest/app/src/main/java/com/example/a20240807listviewtest/MainActivity.java
+++ 20240807ListViewTest/app/src/main/java/com/example/a20240807listviewtest/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a20240807listviewtest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ListView listView=(ListView)findViewById(R.id.citylistView);
+        listView.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView tv =(TextView)findViewById(R.id.textView);
+        String[]citiesArray = getResources().getStringArray(R.array.cities);
+        tv.setText("你點選的是"+citiesArray[i]);
+
     }
 }
