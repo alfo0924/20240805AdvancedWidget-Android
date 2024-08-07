d5a4883f6c52889655f53c19da819c92c352f628
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-07 11:25:21 +0800

    v1.26  客製化 Adapter

---
+++ 20240807ListViewTest/app/src/main/java/com/example/a20240807listviewtest/City.java
@@ -0,0 +1,13 @@
+package com.example.a20240807listviewtest;
+
+public class City {
+
+    private String name;
+    private int zipCode;
+
+    public City(String name ,int zipCode){
+        this.name=name;
+        this.zipCode=zipCode;
+
+    }
+}
