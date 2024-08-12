be55ef69c3b2ca372e69d6df11879aebc365bc1f
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-12 11:20:36 +0800

    v1.212 add delete to do list button function cnp

--- 20240812MidTermSimTest/app/src/main/java/com/example/a20240812midtermsimtest/MainActivity.java
+++ 20240812MidTermSimTest/app/src/main/java/com/example/a20240812midtermsimtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int ADD_TODO_REQUEST = 1;
     private ArrayList<String> todoList;
     private ArrayAdapter<String> adapter;
+    private int selectedPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +56,31 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedPosition = position;
+            }
+        });
+
         Button addButton = findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, toDoList.class);
                 startActivityForResult(intent, ADD_TODO_REQUEST);
+            }
+        });
+
+        Button deleteButton = findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedPosition != -1) {
+                    todoList.remove(selectedPosition);
+                    adapter.notifyDataSetChanged();
+                    selectedPosition = -1;
+                }
             }
         });
     }
